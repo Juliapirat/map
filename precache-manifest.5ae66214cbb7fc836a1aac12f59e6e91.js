c622a675e093d5ca46824064be8b5bf95b4b2acd
Code Sandbox <hello@codesandbox.io>
2022-07-31 09:44:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5ae66214cbb7fc836a1aac12f59e6e91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc83a5bb9508ce120dc6eef0a16131b",
+    "url": "https://juliapirat.github.io/csb-wludfc/index.html"
+  },
+  {
+    "revision": "f50f818587e996921602",
+    "url": "https://juliapirat.github.io/csb-wludfc/static/css/main.eaaec275.chunk.css"
+  },
+  {
+    "revision": "d2e58daa35abf0fdcf75",
+    "url": "https://juliapirat.github.io/csb-wludfc/static/js/2.09ddee8b.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://juliapirat.github.io/csb-wludfc/static/js/2.09ddee8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50f818587e996921602",
+    "url": "https://juliapirat.github.io/csb-wludfc/static/js/main.074ab3ac.chunk.js"
+  },
+  {
+    "revision": "2a285757613165971f23",
+    "url": "https://juliapirat.github.io/csb-wludfc/static/js/runtime-main.3df44e6f.js"
+  }
+]);
